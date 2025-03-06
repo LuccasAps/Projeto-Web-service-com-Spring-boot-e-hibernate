a7a1542f3f6b928d03de2f662ca99a5595f09ccc
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-06 16:22:52 -0300

    OrderItem, many-to-many association with extra attributes - PARTE 2

--- course/src/main/java/com/example/course/entities/Order.java
+++ course/src/main/java/com/example/course/entities/Order.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -26,6 +28,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
@@ -59,6 +64,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     public OrderStatus getOrderStatus() {
