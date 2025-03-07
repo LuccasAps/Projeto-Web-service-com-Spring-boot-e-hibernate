354c371ea631768f29e21053dbf16318494923da
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-07 11:01:59 -0300

    Inserção de User

--- course/src/main/java/com/example/course/resources/UserResource.java
+++ course/src/main/java/com/example/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import com.example.course.entities.User;
@@ -7,6 +8,7 @@ import com.example.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -25,5 +27,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
